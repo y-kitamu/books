7686411cdca31b225deb1ed5ac2e52f617d62aea
Kitamura <ymyk6602@gmail.com>
2022-03-22 21:31:24 +0900

    finish chapter 3.1

--- ar_vr_webgl/src/index.ts
+++ ar_vr_webgl/src/index.ts
@@ -1,75 +1,91 @@
-const vsSource = `
-attribute vec4 vertex_points;
+const drawCanvas = () => {
+  /*========== Create a WebGL Context ==========*/
+  const root = document.getElementById("root") as HTMLDivElement;
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  canvas.setAttribute("width", `${root.clientWidth}`);
+  canvas.setAttribute("height", `${root.clientHeight}`);
+  const gl = canvas?.getContext("webgl");
+  if (!gl) {
+    console.log("WebGL unavailable");
+    return;
+  }
+  /*========== Define and Store the Geometry ==========*/
+  /*====== Define front-face vertices ======*/
+  // prettier-ignore
+  const squares = [
+    // front face
+    -0.3, -0.3, -0.3,
+    0.3, -0.3, -0.3,
+    0.3, 0.3, -0.3,
+      -0.3, -0.3, -0.3,
+      -0.3,  0.3, -0.3,
+    0.3, 0.3, -0.3,
+    // back face
+      -0.2, -0.2, 0.3,
+    0.4, -0.2, 0.3,
+    0.4, 0.4, 0.3,
+      -0.2, -0.2, 0.3,
+      -0.2, 0.4, 0.3,
+    0.4, 0.4, 0.3,
+  ];
+  /*====== Define front-face buffer ======*/
+  // prepare buffer data (vbo : データをGPUのbuffer上に置く)
+  const origBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, origBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(squares), gl.STATIC_DRAW);
+
+  /*========== Shaders ==========*/
+  /*====== Define shader source ======*/
+  const vsSource = `
+attribute vec4 aPosition;
 
 void main() {
-gl_Position = vertex_points;
+gl_Position = aPosition;
 }
 `;
 
-const fsSource = `
+  const fsSource = `
 precision mediump float;
 void main () {
 gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
 `;
-
-const drawCanvas = () => {
-  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-  const root = document.getElementById("root") as HTMLDivElement;
-  console.log(root.clientWidth, root.clientHeight);
-  canvas.setAttribute("width", `${root.clientWidth}`);
-  canvas.setAttribute("height", `${root.clientHeight}`);
-  const gl = canvas?.getContext("webgl");
-
-  if (!gl) {
-    console.log("WebGL unavailable");
-    return;
-  }
-  // clear canvas
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // create Shader
+  /*====== Create shaders ======*/
   const vertexShader = gl.createShader(gl.VERTEX_SHADER);
   const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
   if (!vertexShader || !fragmentShader) {
     console.log("Failed to create shader.");
     return;
   }
+  /*====== Compile shaders ======*/
   gl.shaderSource(vertexShader, vsSource);
   gl.compileShader(vertexShader);
   gl.shaderSource(fragmentShader, fsSource);
   gl.compileShader(fragmentShader);
-  //
+  /*====== Create shader program ======*/
   const program = gl.createProgram();
   if (!program) {
     console.log("Failed to create gl program.");
     return;
   }
+  /*====== Link shader program ======*/
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
-  console.log("Finish shader compile!");
-
-  // prepare buffer data (vbo : データをGPUのbuffer上に置く)
-  const coordinates = [-0.7, 0.7, -0.7, 0.0, 0.7, 0.0];
-  const pointsBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, pointsBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coordinates), gl.STATIC_DRAW);
-
-  // prepare vao (vboのデータに意味をつける)
-  const pointsAttributeLocation = gl.getAttribLocation(
-    program,
-    "vertex_points"
-  );
-  gl.vertexAttribPointer(pointsAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(pointsAttributeLocation);
-
-  // draw
   gl.useProgram(program);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
-
-  console.log(gl.drawingBufferWidth, gl.drawingBufferHeight);
+  /*====== Connect the attribute with the vertex shader =======*/
+  // prepare vao (vboのデータに意味をつける)
+  const pointsAttributeLocation = gl.getAttribLocation(program, "aPosition");
+  gl.vertexAttribPointer(pointsAttributeLocation, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(pointsAttributeLocation);
+  /*========== Drawing ========== */
+  /*====== Draw the points to the screen ======*/
+  // clear canvas
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  // draw
+  gl.drawArrays(gl.TRIANGLES, 0, 12);
+  // gl.drawArrays(gl.LINE_LOOP, 0, 12);
 };
 
 drawCanvas();
