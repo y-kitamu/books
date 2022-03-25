89dd36a6ad660e0ba1240f33a39146422f89ea96
Kitamura <ymyk6602@gmail.com>
2022-03-25 23:16:27 +0900

    wip chapter5

--- ar_vr_webgl/src/index.ts
+++ ar_vr_webgl/src/index.ts
@@ -1,4 +1,5 @@
 import { mat4 } from "gl-matrix";
+import * as THREE from "three";
 
 const drawCanvas = () => {
   /*========== Create a WebGL Context ==========*/
@@ -209,4 +210,72 @@ gl_FragColor = vColor;
   requestAnimationFrame(render);
 };
 
-drawCanvas();
+const drawCanvasWithThree = () => {
+  // create the context
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  const gl = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  // create and set the camera
+  const angleOfView = 55;
+  const aspectRatio = canvas.clientWidth / canvas.clientHeight;
+  const nearPlane = 0.1;
+  const farPlane = 100;
+  const camera = new THREE.PerspectiveCamera(
+    angleOfView,
+    aspectRatio,
+    nearPlane,
+    farPlane
+  );
+  camera.position.set(0, 8, 30);
+  // create the scene
+  const scene = new THREE.Scene();
+  // add fog later...
+  // GEOMETRY
+  const cubeSize = 4;
+  const cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+  // Create the upright plane
+  // Create the cube
+  // Create the Sphere
+  // MATERIALS and TEXTURES
+  const cubeMaterial = new THREE.MeshPhongMaterial({ color: "pink" });
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  cube.position.set(cubeSize + 1, cubeSize + 1, 0);
+  scene.add(cube);
+
+  //LIGHTS
+  const color = 0xffffff;
+  const intensity = 1;
+  const light = new THREE.DirectionalLight(color, intensity);
+  scene.add(light); // MESH
+  // DRAW
+  const draw = () => {
+    if (resizeGLToDisplaySize(gl)) {
+      const canvas = gl.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    cube.rotation.z += 0.01;
+    gl.render(scene, camera);
+    requestAnimationFrame(draw);
+  };
+  requestAnimationFrame(draw);
+  // SET ANIMATION LOOP
+  // UPDATE RESIZE
+};
+
+const resizeGLToDisplaySize = (gl: THREE.WebGLRenderer) => {
+  const canvas = gl.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+  const needResize = canvas.width != width || canvas.height != height;
+  if (needResize) {
+    gl.setSize(width, height, false);
+  }
+  return needResize;
+};
+
+drawCanvasWithThree();
